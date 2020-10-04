c86f4e5bc53ade6c447cebd87b677e0f4323cfc5
repeating <37001921+repeating@users.noreply.github.com>
2020-10-04 18:38:34 +0300

    Add 'initialize' functionality

--- storage_server.py
+++ storage_server.py
@@ -27,38 +27,36 @@ def initialize():
 
 	return availabe_size
 
-
 # Create a socket object
-port = 8080                  
-s = socket.socket()             
+port = 8080
+s = socket.socket()
 ip = socket.gethostbyname(socket.gethostname())   
 
 # Bind to the port
 s.bind((ip, port))
 
 # Now wait for client connection.
-s.listen(5)                     
+s.listen(5)
 print('The server is running on the private ip:', ip)	
 print('Listening on port:', port)
 print('-----------')
 
 while True:
 	# Establish connection with client.
-    c, addr = s.accept()    
-    
-    # receive the file name
-    command = c.recv(1024)
-    command = command.decode()
+	c, addr = s.accept()    
 
-    if command == 'initialize':
-    	initialize()
+	# receive the file name
+	command = c.recv(1024)
+	command = command.decode()
+
+	if command == 'initialize':
+		initialize()
 
 
-    '''
+	'''
 	!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!TO be done later!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 	excute the command
 	'''
 
-    s.shutdown(socket.SHUT_WR)
-    s.close()how to shutdown a port in linux
-	
+	s.shutdown(socket.SHUT_WR)
+	s.close()
